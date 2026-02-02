6c84b8305c98a7abebbf1c9c291097a52279aa7d
OKUMURA Yoshifumi <y-okumura@picosystem.co.jp>
2026-02-02 14:47:58 +0900

    Add locals.tf

---
+++ terraform/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  pgsql_version              = "17"
+  pgsql_storage_mb           = "32768"
+  dtrack_apiserver_image_tag = "dependencytrack/apiserver:latest"
+  dtrack_frontend_image_tag  = "dependencytrack/frontend:latest"
+}
