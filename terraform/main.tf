5c42f4c7177a59acf6d321bfaa2ee03f3a370b1a
OKUMURA Yoshifumi <y-okumura@picosystem.co.jp>
2026-02-03 15:54:57 +0900

    Using Azure Container Apps.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,17 @@ resource "azurerm_resource_group" "this" {
 }
 
 ##
+## Log Analytics Workspace
+##
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "${var.prefix}-law"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+##
 ## Automationアカウント
 ##
 resource "azurerm_automation_account" "this" {
@@ -40,7 +51,7 @@ resource "azurerm_automation_schedule" "workday_morning" {
   interval                = 1
   timezone                = "Asia/Tokyo"
   week_days               = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-  start_time              = "2026-01-01T08:50:00+09:00"
+  start_time              = local.next_0850
 }
 resource "azurerm_automation_schedule" "evening" {
   name                    = "${var.prefix}-evening"
@@ -49,11 +60,11 @@ resource "azurerm_automation_schedule" "evening" {
   frequency               = "Day"
   interval                = 1
   timezone                = "Asia/Tokyo"
-  start_time              = "2026-01-01T18:50:00+09:00"
+  start_time              = local.next_1850
 }
 
 ##
-## VNET 
+## VNET
 ##
 resource "azurerm_virtual_network" "this" {
   resource_group_name = azurerm_resource_group.this.name
@@ -61,11 +72,18 @@ resource "azurerm_virtual_network" "this" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.1.0.0/16"]
 }
+resource "azurerm_subnet" "default" {
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  name                 = "default"
+  address_prefixes     = ["10.1.0.0/24"]
+}
 resource "azurerm_subnet" "pgsql" {
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   name                 = "pgsql"
-  address_prefixes     = ["10.1.0.0/24"]
+  address_prefixes     = ["10.1.16.0/24"]
+
   delegation {
     name = "pgsql"
     service_delegation {
@@ -80,7 +98,17 @@ resource "azurerm_subnet" "container" {
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   name                 = "container"
-  address_prefixes     = ["10.1.1.0/24"]
+  address_prefixes     = ["10.1.32.0/23"]
+
+  delegation {
+    name = "containerapp"
+    service_delegation {
+      name = "Microsoft.App/environments"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
 }
 
 ##
@@ -90,14 +118,21 @@ resource "azurerm_subnet" "container" {
 resource "azurerm_private_dns_zone" "this" {
   resource_group_name = azurerm_resource_group.this.name
   name                = "${var.prefix}.postgres.database.azure.com"
+
 }
 
+# VNETとPrivate DNS zoneのリンク
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   name                  = "${var.prefix}.internal"
   private_dns_zone_name = azurerm_private_dns_zone.this.name
   virtual_network_id    = azurerm_virtual_network.this.id
   resource_group_name   = azurerm_resource_group.this.name
-  depends_on            = [azurerm_subnet.pgsql]
+  registration_enabled  = true
+
+  depends_on = [
+    azurerm_subnet.pgsql,
+    azurerm_subnet.container
+  ]
 }
 
 ##
@@ -106,7 +141,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
 resource "azurerm_postgresql_flexible_server" "this" {
   resource_group_name           = azurerm_resource_group.this.name
   location                      = var.location
-  name                          = module.naming.postgresql_server.name_unique
+  name                          = "${var.prefix}-${module.naming.postgresql_server.name_unique}"
   version                       = local.pgsql_version
   administrator_login           = var.pgsql_login
   administrator_password        = var.pgsql_password
@@ -116,7 +151,11 @@ resource "azurerm_postgresql_flexible_server" "this" {
   delegated_subnet_id           = azurerm_subnet.pgsql.id
   private_dns_zone_id           = azurerm_private_dns_zone.this.id
   backup_retention_days         = "7"
-  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.this]
+  zone                          = "1"
+
+  depends_on = [
+    azurerm_private_dns_zone_virtual_network_link.this
+  ]
 }
 resource "azurerm_postgresql_flexible_server_database" "this" {
   name      = "${var.prefix}-db"
@@ -126,117 +165,143 @@ resource "azurerm_postgresql_flexible_server_database" "this" {
 
   # prevent the possibility of accidental data loss
   lifecycle {
-    prevent_destroy = true
+    # prevent_destroy = true
   }
+}
+resource "azurerm_monitor_diagnostic_setting" "pgsql" {
+  name                       = "${var.prefix}-diagnostics-pgsql"
+  target_resource_id         = azurerm_postgresql_flexible_server.this.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+
+  enabled_log { category_group = "allLogs" }
+  enabled_log { category_group = "audit" }
+  enabled_metric { category = "allmetrics" }
 }
 
 ##
-## Container Instances
+## Container Apps
 ##
-resource "azurerm_container_group" "this" {
-  name                        = "${var.prefix}-aci"
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.this.name
-  os_type                     = "Linux"
-  ip_address_type             = "Private"
-  subnet_ids                  = [azurerm_subnet.container.id]
-  dns_name_label_reuse_policy = "SubscriptionReuse"
+resource "azurerm_container_app_environment" "this" {
+  name                       = "${var.prefix}-cae"
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.this.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  infrastructure_subnet_id   = azurerm_subnet.container.id
 
-  exposed_port = [
-    {
-      port     = 8080
-      protocol = "TCP"
+  # 名前を指定しないと、applyのたびに再作成されてしまう
+  infrastructure_resource_group_name = "${var.prefix}-rg-${module.naming.container_app_environment.name_unique}"
+
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
+  }
+}
+
+## APIサーバ
+resource "azurerm_container_app" "api" {
+  name                         = "${var.prefix}-container-api"
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name          = azurerm_resource_group.this.name
+  revision_mode                = "Single"
+
+  template {
+    min_replicas               = 0
+    max_replicas               = 1
+    cooldown_period_in_seconds = 90
+
+    container {
+      name   = "apiserver"
+      image  = "${local.image_registry}/${local.dtrack_apiserver_image_tag}"
+      cpu    = 2.25
+      memory = "4.5Gi"
+
+      env {
+        name  = "ALPINE_DATABASE_MODE"
+        value = "external"
+      }
+      env {
+        name  = "ALPINE_DATABASE_URL"
+        value = "jdbc:postgresql://${azurerm_postgresql_flexible_server.this.fqdn}:5432/${azurerm_postgresql_flexible_server_database.this.name}"
+      }
+      env {
+        name  = "ALPINE_DATABASE_DRIVER"
+        value = "org.postgresql.Driver"
+      }
+      env {
+        name  = "ALPINE_DATABASE_USERNAME"
+        value = var.pgsql_login
+      }
+      env {
+        name        = "ALPINE_DATABASE_PASSWORD"
+        secret_name = "db-password"
+      }
     }
+  }
+
+  ingress {
+    external_enabled           = true
+    allow_insecure_connections = false
+    target_port                = 8080
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  secret {
+    name  = "db-password"
+    value = var.pgsql_password
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+## フロントエンドサーバ
+resource "azurerm_container_app" "frontend" {
+  name                         = "${var.prefix}-container-frontend"
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name          = azurerm_resource_group.this.name
+  revision_mode                = "Single"
+
+  template {
+    min_replicas               = 0
+    max_replicas               = 1
+    cooldown_period_in_seconds = 90
+
+    container {
+      name   = "frontend"
+      image  = "${local.image_registry}/${local.dtrack_frontend_image_tag}"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      env {
+        name  = "API_BASE_URL"
+        value = "https://${azurerm_container_app.api.ingress[0].fqdn}"
+      }
+    }
+  }
+
+  ingress {
+    external_enabled           = true
+    allow_insecure_connections = false
+    transport                  = "auto"
+    target_port                = 8080
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_container_app.api
   ]
-
-  container {
-    name   = "apiserver"
-    image  = local.dtrack_apiserver_image_tag
-    cpu    = "2.0"
-    memory = "8.0"
-
-    environment_variables = {
-      ALPINE_DATABASE_MODE = "external"
-      ALPINE_DATABASE_URL  = "jdbc:postgresql://${azurerm_postgresql_flexible_server.this.fqdn}/${azurerm_postgresql_flexible_server_database.this.name}"
-    }
-    secure_environment_variables = {
-      ALPINE_DATABASE_USERNAME = var.pgsql_login
-      ALPINE_DATABASE_PASSWORD = var.pgsql_password
-    }
-  }
-
-  container {
-    name   = "frontend"
-    image  = local.dtrack_frontend_image_tag
-    cpu    = "1.0"
-    memory = "1.0"
-
-    ports {
-      port     = 8080
-      protocol = "TCP"
-    }
-
-    environment_variables = {
-      API_BASE_URL = "http://localhost:8081"
-    }
-  }
-}
-##
-## Application Gateway
-##
-resource "azurerm_application_gateway" "this" {
-  name                = "${var.prefix}-agw"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = var.location
-
-  backend_address_pool {
-    name  = "backendPool"
-    fqdns = [azurerm_container_group.this.fqdn]
-  }
-
-  backend_http_settings {
-    name                  = "httpSettings"
-    cookie_based_affinity = "Disabled"
-    port                  = 8080
-    protocol              = "Http"
-    request_timeout       = 20
-  }
-
-  frontend_ip_configuration {
-    name      = "frontendIP"
-    subnet_id = azurerm_subnet.container.id
-  }
-
-  frontend_port {
-    name = "frontendPort"
-    port = 443
-  }
-
-  gateway_ip_configuration {
-    name      = "appGatewayIpConfig"
-    subnet_id = azurerm_subnet.container.id
-  }
-
-  http_listener {
-    name                           = "httpListener"
-    frontend_ip_configuration_name = "frontendIP"
-    frontend_port_name             = "frontendPort"
-    protocol                       = "Https"
-  }
-
-  request_routing_rule {
-    name                       = "routingRule"
-    rule_type                  = "Basic"
-    http_listener_name         = "httpListener"
-    backend_address_pool_name  = "backendPool"
-    backend_http_settings_name = "httpSettings"
-  }
-
-  sku {
-    name     = "Basic"
-    tier     = "Basic"
-    capacity = 1
-  }
 }
 
 ##
@@ -252,50 +317,20 @@ resource "azurerm_automation_runbook" "start" {
   log_progress            = true
   runbook_type            = "PowerShell"
 
-  # 参考リンク
-  # https://learn.microsoft.com/en-us/powershell/module/az.postgresql/start-azpostgresqlflexibleserver?view=azps-15.2.0
-  # https://shisho.dev/dojo/providers/azurerm/Automation/azurerm-automation-job-schedule/
-  # https://learn.microsoft.com/en-us/powershell/module/az.network/start-azapplicationgateway?view=azps-15.2.0
-
   content = <<_EOS_
 param(
-  [Parameter(Mandatory=$true)] [string] $SubscriptionId,
-  [Parameter(Mandatory=$true)] [string] $ResourceGroup,
-
-  # ACI
-  [Parameter(Mandatory=$true)] [string] $AciContainerGroupName,
-
-  # Application Gateway
-  [Parameter(Mandatory=$true)] [string] $AppGwName,
-
-  # PostgreSQL Flexible Server
-  [Parameter(Mandatory=$true)] [string] $PgFlexibleServerName,
-
-  # ACI Start API version (from REST docs)
-  [string] $AciApiVersion = "2025-09-01"
+  [Parameter(Mandatory=$true)] [string] $sid,
+  [Parameter(Mandatory=$true)] [string] $rgname,
+  [Parameter(Mandatory=$true)] [string] $pgsrvname
 )
+Connect-AzAccount -Identity
+Set-AzContext -Subscription $sid
 
-Write-Output "Logging in with Managed Identity..."
-Connect-AzAccount -Identity | Out-Null
-Set-AzContext -Subscription $SubscriptionId | Out-Null
-
-Write-Output "Starting PostgreSQL Flexible Server '$PgFlexibleServerName' in RG '$ResourceGroup'..."
-Start-AzPostgreSqlFlexibleServer -Name $PgFlexibleServerName -ResourceGroupName $ResourceGroup | Out-Null
-Write-Output "PostgreSQL Flexible Server start submitted."
-
-$aciPath = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.ContainerInstance/containerGroups/$AciContainerGroupName/start?api-version=$AciApiVersion"
-Write-Output "Starting ACI container group '$AciContainerGroupName' in RG '$ResourceGroup'..."
-$aciResp = Invoke-AzRestMethod -Method POST -Path $aciPath
-Write-Output "ACI start request status code: $($aciResp.StatusCode)"
-
-Write-Output "Starting Application Gateway '$AppGwName' in RG '$ResourceGroup'..."
-$appGw = Get-AzApplicationGateway -Name $AppGwName -ResourceGroupName $ResourceGroup
-Start-AzApplicationGateway -ApplicationGateway $appGw | Out-Null
-Write-Output "Application Gateway start submitted."
-
-Write-Output "Done."
-  _EOS_
+# Start PostgreSQL
+Start-AzPostgreSqlFlexibleServer -Name $pgsrvname -ResourceGroupName $rgname
+_EOS_
 }
+
 # 停止
 resource "azurerm_automation_runbook" "stop" {
   name                    = "${var.prefix}-stop"
@@ -305,45 +340,21 @@ resource "azurerm_automation_runbook" "stop" {
   log_verbose             = true
   log_progress            = true
   runbook_type            = "PowerShell"
-  content                 = <<_EOS_
+
+  content = <<_EOS_
 param(
-  [Parameter(Mandatory=$true)] [string] $SubscriptionId,
-  [Parameter(Mandatory=$true)] [string] $ResourceGroup,
-
-  # ACI
-  [Parameter(Mandatory=$true)] [string] $AciContainerGroupName,
-
-  # Application Gateway
-  [Parameter(Mandatory=$true)] [string] $AppGwName,
-
-  # PostgreSQL Flexible Server
-  [Parameter(Mandatory=$true)] [string] $PgFlexibleServerName,
-
-  # ACI Start API version (from REST docs)
-  [string] $AciApiVersion = "2025-09-01"
+  [Parameter(Mandatory=$true)] [string] $sid,
+  [Parameter(Mandatory=$true)] [string] $rgname,
+  [Parameter(Mandatory=$true)] [string] $pgsrvname
 )
+Connect-AzAccount -Identity
+Set-AzContext -Subscription $sid
 
-Write-Output "Logging in with Managed Identity..."
-Connect-AzAccount -Identity | Out-Null
-Set-AzContext -Subscription $SubscriptionId | Out-Null
-
-Write-Output "Stoping PostgreSQL Flexible Server '$PgFlexibleServerName' in RG '$ResourceGroup'..."
-Stop-AzPostgreSqlFlexibleServer -Name $PgFlexibleServerName -ResourceGroupName $ResourceGroup | Out-Null
-Write-Output "PostgreSQL Flexible Server stop submitted."
-
-$aciPath = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.ContainerInstance/containerGroups/$AciContainerGroupName/stop?api-version=$AciApiVersion"
-Write-Output "Stoping ACI container group '$AciContainerGroupName' in RG '$ResourceGroup'..."
-$aciResp = Invoke-AzRestMethod -Method POST -Path $aciPath
-Write-Output "ACI stop request status code: $($aciResp.StatusCode)"
-
-Write-Output "Stoping Application Gateway '$AppGwName' in RG '$ResourceGroup'..."
-$appGw = Get-AzApplicationGateway -Name $AppGwName -ResourceGroupName $ResourceGroup
-Stop-AzApplicationGateway -ApplicationGateway $appGw | Out-Null
-Write-Output "Application Gateway stop submitted."
-
-Write-Output "Done."
-  _EOS_
+# Stop PostgreSQL
+Stop-AzPostgreSqlFlexibleServer -Name $pgsrvname -ResourceGroupName $rgname
+_EOS_
 }
+
 # スケジュールと紐付け
 resource "azurerm_automation_job_schedule" "start" {
   automation_account_name = azurerm_automation_account.this.name
@@ -352,11 +363,9 @@ resource "azurerm_automation_job_schedule" "start" {
   schedule_name           = azurerm_automation_schedule.workday_morning.name
 
   parameters = {
-    SubscriptionId        = var.subscription_id
-    ResourceGroup         = azurerm_resource_group.this.name
-    AciContainerGroupName = azurerm_container_group.this.name
-    AppGwName             = azurerm_application_gateway.this.name
-    PgFlexibleServerName  = azurerm_postgresql_flexible_server.this.name
+    sid       = var.subscription_id
+    rgname    = azurerm_resource_group.this.name
+    pgsrvname = azurerm_postgresql_flexible_server.this.name
   }
 }
 resource "azurerm_automation_job_schedule" "stop" {
@@ -366,10 +375,8 @@ resource "azurerm_automation_job_schedule" "stop" {
   schedule_name           = azurerm_automation_schedule.evening.name
 
   parameters = {
-    SubscriptionId        = var.subscription_id
-    ResourceGroup         = azurerm_resource_group.this.name
-    AciContainerGroupName = azurerm_container_group.this.name
-    AppGwName             = azurerm_application_gateway.this.name
-    PgFlexibleServerName  = azurerm_postgresql_flexible_server.this.name
+    sid       = var.subscription_id
+    rgname    = azurerm_resource_group.this.name
+    pgsrvname = azurerm_postgresql_flexible_server.this.name
   }
 }
